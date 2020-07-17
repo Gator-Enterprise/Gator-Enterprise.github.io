e11eb5d6a515ced022ca5c04cfad0c0a067d7257
Brice Shatzer <brice.shatzer@gmail.com>
2020-07-17 12:42:30 -0500

    made golf carts page work

--- scripts/golf-cart-page.js
+++ scripts/golf-cart-page.js
@@ -25,11 +25,60 @@ function menuToggleInit(){
 
 jQuery(document).ready(function($) {
     menuToggleInit();
-    var $activeImage = $('.golf-carts-photos .active');
-    cycleImage();
-    setInterval(function(){
+    
+    const SPACE_ID = 'oo4yz13ylbb3';
+    const ACCESS_TOKEN = 'f9e0e09a35ebd58e104234e7b9c1460462edc4ec1ef036237b9f72b31f7f39ac';
+
+    const client = contentful.createClient({
+        space: SPACE_ID,
+        accessToken: ACCESS_TOKEN
+    });
+
+    const $activeImage = $('.golf-carts-photos .active');
+
+    const $availableCarts = $('<div>');
+    
+    // client.getEntries()
+    client.getAssets()
+        .then((response) => {
+            let items = response.items;
+            if( items.length > 0 ){  // - has items
+                $('.no-carts').hide();
+
+                $.each(items,function(i) {  // -- for each item
+                    const fields = this.fields;
+                    const altTag = this.fields.file.title || 'Golf Cart For Sale';
+                    let $cart = $('<img src="'+this.fields.file.url+'" class="cart" alt="'+this.fields.file.title+'"></img>');
+                    $availableCarts.append($cart);
+                }); // -- end each item
+
+                $('.available-carts').append($availableCarts);
+
+                const $info = $(
+                    '<div class="info"><h3>Available Carts</h3><p>All carts are freshly serviced and ready to hit the course! Looking for a recreational ride? We can outfit any of these carts with flip-back seats, lights, lift-kits and more!</p></div>'
+                );
+
+                $availableCarts.prepend($info);
+            // -- has items
+            } else {
+                initEmpty(); 
+            }
+
+        })
+        .catch((error) => {
+            console.log('error occured');
+            console.log(error);
+            initEmpty();
+        });
+
+
+    function initEmpty() {
         cycleImage();
-    }, 3500);
+        setInterval(function(){
+            cycleImage();
+        }, 3500);        
+    }
+
 
     function cycleImage() {
         $('.golf-carts-photos img').removeClass('active');
@@ -43,9 +92,7 @@ jQuery(document).ready(function($) {
 
         $activeImage = $nextImage;
     }
-
-
-
+            
 
 });
 
